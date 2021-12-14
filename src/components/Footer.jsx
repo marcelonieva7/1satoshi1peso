72ed9082e3939b923b6150c9b9c2ff09ee1f0dbd
Marcelo Nieva <marcelonieva@gmail.com>
2021-12-13 21:59:51 -0300

    add Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { footer } from '../assets/styles/Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={footer}>
+      © Todos los derechos reservados - 1 Satoshi 1 Peso 2021
+    </footer>
+  );
+}
+ 
+export default Footer;
