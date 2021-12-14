3d49d709dcee4caf4f3cc3ee7d985064f6aa7f7b
Marcelo Nieva <marcelonieva@gmail.com>
2021-12-13 22:00:02 -0300

    update index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Depreciation from './components/Depreciation';
+import BetForm from './components/BetForm';
+import Footer from './components/Footer';
+import Merchandising from './components/Merchandising';
+import Hero2 from './components/Hero2';
+import './assets/styles/Index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>HOLAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
+    <Header />
+    <main>
+      <Hero />
+      <Depreciation />
+      <Hero2 />
+      <BetForm />
+      <Merchandising />
+    </main>
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
