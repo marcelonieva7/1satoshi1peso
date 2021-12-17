93d0f5e2c013a31b60cd23846fe7f0770a7a1491
Marcelo Nieva <marcelonieva@gmail.com>
2021-12-17 01:33:32 -0300

    fix issues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { equal } from '../assets/styles/Header.module.css'
 
 const Header = () => {
-  const btcArs = 12596000
-  const btcUsd = 67000
+  const BTCARS = 12596000
+  const BTCUSD = 67000
   const formatCurrency = (number) => Intl.NumberFormat('es-AR').format(number)
   return (
     <header>
       <span>1 BTC</span>
       <span className={equal}> = </span>
-      <span>{formatCurrency(btcUsd)} USD</span>
+      <span>{formatCurrency(BTCUSD)} USD</span>
       <span className={equal}> = </span>
-      <span>{formatCurrency(btcArs)} ARS</span>
+      <span>{formatCurrency(BTCARS)} ARS</span>
       <span className={equal}> = </span>
       <span>100.000.000 SAT</span>
     </header>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
-import useWindowDimensions from '../hooks/useWindowDimensions'
-import ProgressSvg from './ProgressSvg'
+import ProgressSvg from './svg/ProgressSvg'
 import Styles from '../assets/styles/Hero.module.css'
 
 const Hero = () => {
-  const { width } = useWindowDimensions()
   const SATO = 0.1259  // valor de 1 Satoshi en pesos argentinos.
 
   let ratio = 0.0001  // calcula el porcentaje de progreso para llegar a la meta de 1Satoshi = 1 peso.
   if (SATO) {
     SATO > 1 ? ratio = 1 : ratio = SATO
   }
-  const style = width > 1369 ? {left: `calc(((100vw - 1369px) /2) + ${15+(1135*ratio)}px)`} : width < 768 ? {left: `calc((-45px + 6%) + ${90*ratio}% - ${100*ratio}px)`} : {left: `calc((-61px + 5.5% ) + ${92*ratio}% - ${120*ratio}px)`};
   return (
     <section className={Styles.container_hero}>
       <div className={Styles.main} style={{backgroundImage: "url('images/background.svg')"}} >
@@ -27,9 +24,8 @@ const Hero = () => {
           <span>Un satoshi es la unidad mínima de medida que se puede utilizar en el sistema Bitcoin.</span>
           <span>Es la fracción más pequeña en la que puedes dividir un bitcoin.</span>
         </div>
-        <img src="images/horse.svg" alt="Progreso de satoshi." className={Styles.progress_Horse} style={style} />
         <img src="images/hang.svg" alt="Manoapuntando hacia abajo." className={Styles.bounce} />
-        <ProgressSvg className={Styles.progress_Img} ratio={ratio} />
+        <ProgressSvg className={Styles.progressBar} ratio={ratio} />
       </div>
     </section>
   )
