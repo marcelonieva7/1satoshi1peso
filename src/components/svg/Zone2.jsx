9c84dad3e56d483f0a26744d6479ea343aedc006
Marcelo Nieva <marcelonieva@gmail.com>
2021-12-17 02:21:07 -0300

    fix style issue

--- src/components/svg/Zone2.jsx
+++ src/components/svg/Zone2.jsx
@@ -2,20 +2,21 @@ import { Svg } from 'react-svg-path';
 
 const Zone2 = () => (
   <Svg width="100%" height="100%" viewBox="0 0 1526.41 420.7" preserveAspectRatio="none">
-    <polygon stroke="null" id="svg_1" opacity="0.8" fill="#2d3c5c" points="-1.499952793121338,421.67000692791044 1523.168212890625,421.67000692791044 1523.8800048828125,361.14358675097174 1275.97998046875,336.5974464416504 1019.2567749023438,299.38133840313094 760.7589111328125,208.84294270794635 459.1479797363281,110.91958140798306 351.1548156738281,68.54909421070624 227.1195526123047,35.34196136796163 -1.499952793121338,1.6525514703016029 -1.499952793121338,-1 -1.499952793121338,421.67000692791044 " className="cls-1" sx="" sy="" cx="" cy=""/>
-    <polyline stroke="#2d3c5c" id="svg_2" strokeMiterlimit="10" strokeWidth="5px" fill="none" opacity="0.8" points="1523.168212890625,421.67000692791044 1523.8800048828125,361.14358675097174 1275.97998046875,334.447257511114 1015.3565063476562,299.38133840313094 760.7589111328125,208.84294270794635 459.1479797363281,110.91958140798306 351.1548156738281,68.54909421070624 227.1195526123047,35.34196136796163 -1.499952793121338,1.6525514703016029 -1.499952793121338,-1 " className="cls-2" sx="" sy="" cx="" cy=""/>
-    <circle sx="" sy="" stroke="#2d3c5c" id="svg_3" strokeWidth="6px" fill="#fff" strokeMiterlimit="10" r="12.14001" cy="116.55625" cx="501.25841" className="cls-3"/>
-    <line stroke="#2d3c5c" id="svg_4" strokeWidth="6px" fill="#fff" strokeMiterlimit="10" y2="116.55625" x2="-1.49995" y1="116.55625" x1="489.07647" className="cls-3" sx="" sy="" cx="" cy=""/>
-    <circle sx="" sy="" stroke="#2d3c5c" id="svg_5" strokeWidth="6px" fill="#fff" strokeMiterlimit="10" r="12.14001" cy="299.08997" cx="1015.35652" className="cls-3"/>
-    <line stroke="#2d3c5c" id="svg_6" strokeWidth="6px" fill="#fff" strokeMiterlimit="10" y2="299.08997" x2="-1.49995" y1="299.08997" x1="1003.17457" className="cls-3" sx="" sy="" cx="" cy=""/>
-    <circle sx="" sy="" stroke="#2d3c5c" id="svg_7" strokeWidth="6px" fill="#fff" strokeMiterlimit="10" r="12.14001" cy="334.44727" cx="1275.98997" className="cls-3"/>
-    <line stroke="#2d3c5c" id="svg_8" strokeWidth="6px" fill="#fff" strokeMiterlimit="10" y2="334.44727" x2="-1.49995" y1="334.44727" x1="1263.80802" className="cls-3"  sx="" sy="" cx="" cy=""/>
-    <circle sx="" sy="" stroke="#2d3c5c" id="svg_9" strokeWidth="6px" fill="#fff" strokeMiterlimit="10" r="12.14" cy="68.9309" cx="350.76378" className="cls-3"/>
-    <line stroke="#2d3c5c" id="svg_10" strokeWidth="6px" fill="#fff" strokeMiterlimit="10" y2="70.5586" x2="-1.49995" y1="70.5586" x1="338.59186" className="cls-3" sx="" sy="" cx="" cy=""/>
-    <line stroke="#2d3c5c" id="svg_11" strokeWidth="5.92px" fill="#fff" strokeMiterlimit="10" y2="364.65019" x2="-1.49995" y1="364.65019" x1="1397.45849" className="cls-4" sx="" sy="" cx="" cy=""/>
-    <line stroke="null" id="svg_12" y2="21.71749" x2="-1.49995" y1="41.47096" x1="-1.49995" sx="" sy="" cx="" cy=""/>
-    <circle sx="" sy="" stroke="#2d3c5c" id="svg_13" strokeWidth="6px" fill="#fff" strokeMiterlimit="10" r="12.14" cy="32.00618" cx="227.11955" className="cls-3"/>
-    <line stroke="#2d3c5c" id="svg_14" strokeWidth="6px" fill="#fff" strokeMiterlimit="10" y2="32.19708" x2="-1.49995" y1="32.19708" x1="214.94763" className="cls-3" sx="" sy="" cx="" cy=""/>
+    <polygon cx="" cy="" sx="" sy="" stroke="null" id="svg_1" opacity="0.8" fill="#2d3c5c" points="-0.500144064387321,422.6700134277344 1524.168119070996,422.6700134277344 1524.8800115537624,362.1435852050781 1276.9799447332362,337.5974426269531 1020.2566497449981,300.38134765625 761.7588149414906,209.8429412841797 460.1478772407819,111.9195785522461 352.1546854104955,69.5490951538086 228.1193910268331,36.34196090698242 -0.500144064387321,2.6525516510009766 -0.500144064387321,0 -0.500144064387321,422.6700134277344 " className="cls-1"/>
+    <polyline cx="" cy="" sx="" sy="" stroke="#2d3c5c" id="svg_2" strokeMiterlimit="10" strokeWidth="5px" fill="none" opacity="0.8" points="1524.168119070996,422.6700134277344 1524.8800115537624,362.1435852050781 1276.9799447332362,335.447265625 1016.356509888744,300.38134765625 761.7588149414906,209.8429412841797 460.1478772407819,111.9195785522461 352.1546854104955,69.5490951538086 228.1193910268331,36.34196090698242 -0.500144064387321,2.6525516510009766 -0.500144064387321,0 " className="cls-2"/>
+    <circle sx="" sy="" stroke="#2d3c5c" id="svg_3" strokeWidth="6px" fill="#fff" strokeMiterlimit="10" r="12.14001" cy="117.55625" cx="502.25829" className="cls-3"/>
+    <line cx="" cy="" sx="" sy="" stroke="#2d3c5c" id="svg_4" strokeWidth="6px" fill="#fff" strokeMiterlimit="10" y2="117.55625" x2="-0.50014" y1="117.55625" x1="490.07634" className="cls-3"/>
+    <circle sx="" sy="" stroke="#2d3c5c" id="svg_5" strokeWidth="6px" fill="#fff" strokeMiterlimit="10" r="12.14001" cy="212.20411" cx="762.69125" className="cls-3"/>
+    <line cx="" cy="" sx="" sy="" stroke="#2d3c5c" id="svg_6" strokeWidth="6px" fill="#fff" strokeMiterlimit="10" y2="212.20411" x2="-0.50014" y1="212.20411" x1="750.5093" className="cls-3"/>
+    <circle sx="" sy="" stroke="#2d3c5c" id="svg_7" strokeWidth="6px" fill="#fff" strokeMiterlimit="10" r="12.14001" cy="300.08997" cx="1016.35646" className="cls-3"/>
+    <line cx="" cy="" sx="" sy="" stroke="#2d3c5c" id="svg_8" strokeWidth="6px" fill="#fff" strokeMiterlimit="10" y2="300.08997" x2="-0.50014" y1="300.08997" x1="1004.17451" className="cls-3"/>
+    <circle sx="" sy="" stroke="#2d3c5c" id="svg_9" strokeWidth="6px" fill="#fff" strokeMiterlimit="10" r="12.14001" cy="335.44727" cx="1276.98994" className="cls-3"/>
+    <line cx="" cy="" sx="" sy="" stroke="#2d3c5c" id="svg_10" strokeWidth="6px" fill="#fff" strokeMiterlimit="10" y2="335.44727" x2="-0.50014" y1="335.44727" x1="1264.80799" className="cls-3"/>
+    <circle sx="" sy="" stroke="#2d3c5c" id="svg_11" strokeWidth="6px" fill="#fff" strokeMiterlimit="10" r="12.14" cy="69.9309" cx="351.76363" className="cls-3"/>
+    <line cx="" cy="" sx="" sy="" stroke="#2d3c5c" id="svg_12" strokeWidth="6px" fill="#fff" strokeMiterlimit="10" y2="71.5586" x2="-0.50014" y1="71.5586" x1="339.59171" className="cls-3"/>
+    <line cx="" cy="" sx="" sy="" stroke="#2d3c5c" id="svg_13" strokeWidth="5.92px" fill="#fff" strokeMiterlimit="10" y2="365.65019" x2="-0.50014" y1="365.65019" x1="1398.45848" className="cls-4"/>
+    <circle sx="" sy="" stroke="#2d3c5c" id="svg_14" strokeWidth="6px" fill="#fff" strokeMiterlimit="10" r="12.14" cy="33.00618" cx="228.11939" className="cls-3"/>
+    <line cx="" cy="" sx="" sy="" stroke="#2d3c5c" id="svg_15" strokeWidth="6px" fill="#fff" strokeMiterlimit="10" y2="33.19708" x2="-0.50014" y1="33.19708" x1="215.94747" className="cls-3"/>
   </Svg>
 )
 
